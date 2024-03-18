425f1f6544cd0a34f18c8e2f967fa357960ca25e
jinho <qmfforflr@naver.com>
2024-03-18 16:05:56 +0900

    프로그래머스 : 자릿수 더하기
    programmers : Sum of Digits

---
+++ Sum of Digits.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        answer += n / 100000000;
+        n %= 100000000;
+        answer += n / 10000000;
+        n %= 10000000;
+        answer += n / 1000000;
+        n %= 1000000;
+        answer += n / 100000;
+        n %= 100000;
+        answer += n / 10000;
+        n %= 10000;
+        answer += n / 1000;
+        n %= 1000;
+        answer += n / 100;
+        n %= 100;
+        answer += n / 10;
+        n %= 10;
+        answer += n / 1;
+
+        return answer;
+    }
+}
